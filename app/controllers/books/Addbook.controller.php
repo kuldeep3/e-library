1e8b3b272b752bfa9f6aaac49cf06b0786425a3f
kuldeep3 <kuldeepupreti3@gmail.com>
2020-02-21 14:34:52 +0530

    recent changes

--- app/controllers/books/Addbook.controller.php
+++ app/controllers/books/Addbook.controller.php
@@ -40,13 +40,16 @@
             $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
             && $imageFileType != "gif"
         ) {
-            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
             $uploadOk = 0;
+            $err = "Sorry, only JPG, JPEG, PNG & GIF files are allowed";
+            $_SESSION["err"] = $err;
+            header('location:/addBook');
         }
         // Check if $uploadOk is set to 0 by an error
         if ($uploadOk == 0) {
-            echo "Sorry, your file was not uploaded.";
-            // if everything is ok, try to upload file
+            $err = "Sorry, your file was not uploaded";
+            $_SESSION["err"] = $err;
+            header('location:/addBook');
         } else {
             $i = 1;
             $categories = [];
@@ -61,10 +64,10 @@
                     $stmt = App::get('databaseBook')->addCategories($book_id, $categories);
                     header('location:/bookaddedmsg');
                 }
-
-                echo "The file " . basename($_FILES["image"]["name"]) . " has been uploaded.";
             } else {
-                echo "Sorry, there was an error uploading your file.";
+                $err = "Sorry, there was an error uploading your file";
+                $_SESSION["err"] = $err;
+                header('location:/addBook');
             }
         }
     }
